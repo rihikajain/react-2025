59769767482dd51a14553d3c26a1ce8cfcd1e25e
Rihika <116342520+rihikajain@users.noreply.github.com>
2025-10-02 15:13:21 +0530

    updates

--- 09-proj-blogsite/src/appwrite/auth.js
+++ 09-proj-blogsite/src/appwrite/auth.js
@@ -14,15 +14,33 @@ export class AuthService {
 
   async createAccount({ email, password, name }) {
     const userAccount = await this.account.create(ID.unique(), email, password, name);
-    if(userAccount){
-      this.login({email,password})
+    if (userAccount) {
+      this.login({ email, password })
     }
     else
-    return userAccount;
+      return userAccount;
   }
 
-  async login({email,password}){
-    return await this.account.createEmailPasswordSession(email,password)
+  async login({ email, password }) {
+    return await this.account.createEmailPasswordSession(email, password)
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get()
+    }
+    catch (e) {
+      console.log("error:", e)
+    }
+    return null
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions()
+    } catch (error) {
+      console.log(error)
+    }
   }
 
 }
