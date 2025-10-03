076cc7782150b2ed5b6d4dc74a3c3e0f06f69a12
Rihika <116342520+rihikajain@users.noreply.github.com>
2025-10-04 04:34:29 +0530

    updates

--- 11-proj-talker/src/App.jsx
+++ 11-proj-talker/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { GoogleGenAI } from "@google/genai";
+import { ChevronRight, ChevronLeft } from 'lucide-react';
 
 function App() {
   const [question, setQuestion] = useState('')
@@ -18,7 +19,7 @@ function App() {
         // }
       });
       console.log(response);
-      setResult((prev) => [...prev, response.text.replace(/\*\*/g, '')])
+      setResult((prev) => [...prev, response.text.split('*')])
     } catch (error) {
       console.error("API Error:", error);
     }
@@ -27,33 +28,37 @@ function App() {
   const clicked = () => {
 
   }
+
+  let [open, setOpen] = useState(true)
   // bg-zinc-800
   return (
     <main className=" h-screen ">
-      <div className="grid grid-cols-5 h-screen text-center">
-        <div className="col-span-1 bg-zinc-600">
+      <div className={`grid ${open ? 'grid-cols-5' : 'grid-cols-1'} h-screen text-center`}>
+        <button className="mt-10 absolute left-10 z-10" onClick={() => setOpen(!open)}>{!open ? <ChevronRight /> : <ChevronLeft />}</button>
+        {open && (<div className={`col-span-1} bg-zinc-200 relative`}>
+          <div></div>
 
-        </div>
+        </div>)}
 
-        <div className="col-span-4 p-10  ">
-          <div className="container h-18/20  rounded-4xl mb-5 text-white pt-10 flex flex-col">
+        {(<div className={`col-span-${open ? '4' : 'full'} p-10`}>
+          <div className=" h-18/20 w-full  rounded-4xl mb-5  text-white pt-10 flex flex-col">
             {question && (
               <div className="flex justify-end">
-                <p className=" rounded-xl p-2 bg-[#1689fc] text-black m-2 text-end  justify-end">{question}</p>
+                <p className=" rounded-xl p-2 bg-[#1689fc] overflow-auto text-black mr-0 mb-2 text-end max-w-xl">{question}</p>
               </div>
             )}
 
             {result.map((d, i) => {
               return (
-              <p key={i} className="flex justify-start rounded-xl p-2 bg-[#d3a826] text-black m-2 text-start">{d}</p>)
+                <p key={i} className="max-w-xl rounded-xl p-2 bg-[#d3a826] text-black m-2 text-start">{d}</p>)
             })}
           </div>
-          <div className="bg-zinc-900  h-2/20 w-1/2 p-1 pr-5 text-white m-auto rounded-4xl border border-zinc-700 flex h-16 ">
+          <div className="bg-zinc-200  h-2/20 w-1/2 p-1 pr-5 text-black m-auto rounded-4xl border border-zinc-700 flex h-16 ">
             <input type="text" placeholder="Ask me anything" onChange={(e) => setQuestion(e.target.value)} className="w-full h-full p-3 outline-none" />
             <button className="hover:scale-90 transition-all" onClick={handleAsk}>Ask</button>
           </div>
-        </div>
-
+        </div>)
+        }
       </div>
     </main>
   )
